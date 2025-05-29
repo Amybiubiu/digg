2f1f9c691348c3d182f64be66f2d3c8b2c6d2c32
TimBao <dabao0521@gmail.com>
2025-05-29 14:51:55 +0800

    fix:详情页没有评论的时候，文案改成“暂无评论，写评论”，其中“写评论”的颜色是蓝色，支持点击唤起评论框

--- digg/Articale/SLEmptyCommentCell.m
+++ digg/Articale/SLEmptyCommentCell.m
@@ -38,32 +38,32 @@
     [self.contentView addSubview:self.sectionSegment];
     
     // 创建左侧横线
-    UIView *leftLineView = [[UIView alloc] init];
-    leftLineView.backgroundColor = Color16(0xC6C6C6);
-    [self.contentView addSubview:leftLineView];
+//    UIView *leftLineView = [[UIView alloc] init];
+//    leftLineView.backgroundColor = Color16(0xC6C6C6);
+//    [self.contentView addSubview:leftLineView];
     
     // 创建提示标签
     self.emptyLabel = [[UILabel alloc] init];
-    self.emptyLabel.text = @"还没有评论";
+    self.emptyLabel.text = @"暂无评论";
     self.emptyLabel.textColor = Color16(0x999999);
     self.emptyLabel.font = [UIFont pingFangRegularWithSize:12];
     self.emptyLabel.textAlignment = NSTextAlignmentCenter;
     [self.contentView addSubview:self.emptyLabel];
 
     // 创建右侧横线
-    UIView *rightLineView = [[UIView alloc] init];
-    rightLineView.backgroundColor = Color16(0xC6C6C6);
-    [self.contentView addSubview:rightLineView];
+//    UIView *rightLineView = [[UIView alloc] init];
+//    rightLineView.backgroundColor = Color16(0xC6C6C6);
+//    [self.contentView addSubview:rightLineView];
     
     // 创建评论按钮
     self.commentButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [self.commentButton setTitle:@"立即评论" forState:UIControlStateNormal];
-    [self.commentButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.commentButton setTitle:@"写评论" forState:UIControlStateNormal];
+    [self.commentButton setTitleColor:Color16(0x0066FF) forState:UIControlStateNormal];
     self.commentButton.titleLabel.font = [UIFont pingFangRegularWithSize:12];
-    self.commentButton.backgroundColor = Color16(0x14932A);
-    self.commentButton.layer.cornerRadius = 4;
+    self.commentButton.backgroundColor = [UIColor clearColor];
     [self.commentButton addTarget:self action:@selector(commentButtonTapped) forControlEvents:UIControlEventTouchUpInside];
-    self.commentButton.hidden = YES;
+    // 显示按钮
+    self.commentButton.hidden = NO;
     [self.contentView addSubview:self.commentButton];
     
     [self.sectionSegment mas_updateConstraints:^(MASConstraintMaker *make) {
@@ -73,31 +73,31 @@
 
     // 设置约束
     [self.emptyLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.equalTo(self.contentView);
+        make.centerX.equalTo(self.contentView).offset(-20); // 向左偏移，为写评论按钮留出空间
         make.top.equalTo(self.sectionSegment.mas_bottom).offset(55);
     }];
     
-    // 设置左侧横线约束
-    [leftLineView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.equalTo(self.emptyLabel);
-        make.right.equalTo(self.emptyLabel.mas_left).offset(-10);
-        make.width.mas_equalTo(7);
-        make.height.mas_equalTo(0.5);
-    }];
+//    // 设置左侧横线约束
+//    [leftLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.centerY.equalTo(self.emptyLabel);
+//        make.right.equalTo(self.emptyLabel.mas_left).offset(-15);
+//        make.width.mas_equalTo(7);
+//        make.height.mas_equalTo(0.5);
+//    }];
     
     // 设置右侧横线约束
-    [rightLineView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.equalTo(self.emptyLabel);
-        make.left.equalTo(self.emptyLabel.mas_right).offset(10);
-        make.width.mas_equalTo(7);
-        make.height.mas_equalTo(0.5);
-    }];
+//    [rightLineView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.centerY.equalTo(self.emptyLabel);
+//        make.left.equalTo(self.commentButton.mas_right).offset(10);
+//        make.width.mas_equalTo(7);
+//        make.height.mas_equalTo(0.5);
+//    }];
     
+    // 修改评论按钮位置，放在标签右侧
     [self.commentButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.equalTo(self.contentView);
-        make.top.equalTo(self.emptyLabel.mas_bottom).offset(14);
-        make.width.mas_equalTo(60);
-        make.height.mas_equalTo(24);
+        make.centerY.equalTo(self.emptyLabel);
+        make.left.equalTo(self.emptyLabel.mas_right).offset(5);
+        make.height.equalTo(self.emptyLabel);
     }];
 }
 
